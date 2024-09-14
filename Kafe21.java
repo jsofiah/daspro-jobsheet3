c3c06f6c23da2811dd3b88bb15fee0b179b5c5e5
sofiahdingdong <sofiahdingdong@gmail.com>
2024-09-14 20:48:53 +0700

    percobaan 2

--- Kafe21.java
+++ Kafe21.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
 public class Kafe21 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner kafe = new Scanner(System.in);
         boolean keanggotaan;
         int jmlKopi, jmlTeh, jmlRoti;
         double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga, nominalBayar;
         float diskon = 10 /100f;
 
         System.out.print("Masukkan keanggotaan (true/false): ");
-        keanggotaan = input.nextBoolean();
+        keanggotaan = kafe.nextBoolean();
         System.out.print("Masukkan jumlah pembelian kopi: ");
-        jmlKopi = input.nextInt();
+        jmlKopi = kafe.nextInt();
         System.out.print("Masukkan jumlah pembelian teh: ");
-        jmlTeh = input.nextInt();
+        jmlTeh = kafe.nextInt();
         System.out.print("Masukkan jumlah pembelian roti: ");
-        jmlRoti = input.nextInt();
+        jmlRoti = kafe.nextInt();
 
         totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
         nominalBayar = totalHarga - (diskon * totalHarga);
 
+        int nominalInt = (int) nominalBayar;
+        byte totalByte = (byte) totalHarga;
+
         System.out.println("Keanggotaan pelanggan: " + keanggotaan);
         System.out.println("Item pembelian " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti");
         System.out.println("Nominal bayar Rp " + nominalBayar);
+        System.out.println("Nominal bayar int version Rp " + nominalInt);
+        System.out.println("Total harga: " + totalHarga);
+        System.out.println("Total harga byte version: " + totalByte);
     }
 }
